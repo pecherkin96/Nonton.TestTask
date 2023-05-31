99fa4adc0abae0288aeaa7acae4c2b3fe67fc182
Maksim Pecherkin <maximal96@yandex.ru>
2023-05-31 18:03:23 +0300

    Добавьте файлы проекта.

---
+++ Nonton.TestTask/ProductsImpl.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nonton.TestTask
+{
+    class ProductsImpl
+    {
+        List<Product> Products = new List<Product>();
+
+        public bool AddProduct(Product product)
+        {
+            foreach(Product productCycle in Products)
+            {
+                if(productCycle.id == product.id)
+                {
+                    return false;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Products.Add(product);
+            return true;
+        }
+
+        public bool DeleteProduct(Product product)
+        {
+           if (Products.Contains(product) == true)
+           {
+                Products.Remove(product);
+                return true;
+           }
+           else
+           {
+                return false;
+           }
+        }
+
+        public string GetName(string id)
+        {
+            foreach (Product productCycle in Products)
+            {
+                if (productCycle.id == id)
+                {
+                    return productCycle.name;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return null;
+        }
+
+        public List<string> FindByName(string name)
+        {
+            List<string> listsId = new List<string>();
+
+            foreach(Product productCycle in Products)
+            {
+                if(productCycle.name == name)
+                {
+                    listsId.Add(productCycle.id);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return listsId;
+        }
+    }
+}
+
---
+++ Nonton.TestTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nonton.TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Это тест отработки
+
+            Product product1 = new Product();
+            product1.id = "sdk-1";
+            product1.name = "chair";
+
+            Product product2 = new Product();
+            product2.id = "shj-2";
+            product2.name = "sofa";
+
+            Product product3 = new Product();
+            product3.id = "klmj-20";
+            product3.name = "table";
+
+            ProductsImpl productimpl = new ProductsImpl();
+
+            productimpl.AddProduct(product1);
+            productimpl.AddProduct(product2);
+            productimpl.AddProduct(product1);
+            productimpl.AddProduct(product3);
+            productimpl.DeleteProduct(product1);
+            productimpl.DeleteProduct(product1);
+            productimpl.AddProduct(product1);
+            Console.WriteLine(productimpl.GetName("sdk-1"));
+            Console.WriteLine(String.Join(", ", productimpl.FindByName(productimpl.GetName("shj-2"))));
+
+        }
+    }
+}
